215ea6dc70a7a34268026a450ae8078ca196f349
Nick Rosbrook <nr@enr0n.net>
2021-08-03 19:53:06 -0400

    octet_string: add an octet_string type
    Make a new type, octet_string, that operates sort of like a byte slice.
    These should aid the implementation of the hashing sub functions.
    
    Signed-off-by: Nick Rosbrook <nr@enr0n.net>

---
+++ src/octet_string.c
@@ -0,0 +1,134 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "octet_string.h"
+
+void octet_string_alloc(octet_string **po, uint32_t cap)
+{
+    octet_string *o;
+    o = calloc(1, sizeof(octet_string*));
+
+    o->data = calloc(cap, sizeof(uint8_t));
+    o->cap = cap;
+    o->len = 0;
+
+    *po = o;
+}
+
+void octet_string_free(octet_string *o)
+{
+    if (!o) {
+        return;
+    }
+
+    if (o->data) {
+        free(o->data);
+    }
+
+    free(o);
+}
+
+void octet_string_reset(octet_string *o)
+{
+    for (int i = 0; i < o->cap; i++) {
+        o->data[i] = 0x0;
+    }
+    o->len = 0;
+}
+
+char *octet_string_to_str(octet_string *o)
+{
+    char *str;
+
+    str = calloc(2 * o->len + 1, sizeof(char));
+    for (int i = 0; i < o->len; i++) {
+        sprintf(&str[2*i], "%02x", o->data[i]);
+    }
+    str[2 * o->len] = '\0';
+
+    return str;
+}
+
+static inline void octet_string_realloc(octet_string *o, uint32_t cap)
+{
+    uint8_t *new;
+
+    new = realloc(o->data, cap);
+    assert(new != NULL);
+
+    o->data = new;
+    o->cap = cap;
+}
+
+octet_string *octet_string_append(octet_string *dest, uint8_t byte)
+{
+    assert(dest->cap >= dest->len);
+
+    if (dest->cap == dest->len) {
+        octet_string_realloc(dest, dest->cap + 1);
+    }
+
+    dest->data[dest->len] = byte;
+    dest->len++;
+
+    return dest;
+}
+
+octet_string *octet_string_appendn(octet_string *dest, uint8_t *bytes, uint32_t n)
+{
+    if (dest->cap < dest->len + n) {
+        octet_string_realloc(dest, dest->len + n);
+    }
+
+    for (int i = 0; i < n; i++) {
+        dest->data[dest->len + i] = bytes[i];
+    }
+    dest->len += n;
+
+    return dest;
+}
+
+octet_string *octet_strcpy(octet_string *dest, const octet_string *src)
+{
+    if (src->len > dest->cap) {
+        octet_string_realloc(dest, src->cap);
+    }
+
+    memcpy(dest->data, src->data, src->len);
+    dest->len = src->len;
+
+    assert(dest->cap >= dest->len);
+
+    return dest;
+}
+
+octet_string *octet_strcat(octet_string *dest, const octet_string *src)
+{
+    if (src->len + dest->len > dest->cap) {
+        octet_string_realloc(dest, src->len + dest->len);
+    }
+
+    for (int i = 0; i < src->len; i++) {
+        dest->data[dest->len + i] = src->data[i];
+    }
+    dest->len += src->len;
+
+    assert(dest->cap >= dest->len);
+
+    return dest;
+}
+
+octet_string *octet_strncpy(octet_string *dest, const octet_string *src, uint32_t n)
+{
+    if (n > dest->cap) {
+        octet_string_realloc(dest, n);
+    }
+
+    memcpy(dest->data, src->data, n);
+    dest->len = n;
+
+    return dest;
+}
---
+++ src/octet_string.h
@@ -0,0 +1,26 @@
+#ifndef _OCTET_STRING_H_
+#define _OCTET_STRING_H_
+
+#include <stdint.h>
+
+typedef struct {
+    uint8_t *data;
+
+    uint32_t len;
+    uint32_t cap;
+} octet_string;
+
+void octet_string_alloc(octet_string **o, uint32_t cap);
+void octet_string_free(octet_string *o);
+void octet_string_reset(octet_string *o);
+
+char *octet_string_to_str(octet_string *o);
+
+octet_string *octet_string_append(octet_string *dest, uint8_t byte);
+octet_string *octet_string_appendn(octet_string *dest, uint8_t *bytes, uint32_t n);
+
+octet_string *octet_strcpy(octet_string *dest, const octet_string *src);
+octet_string *octet_strcat(octet_string *dest, const octet_string *src);
+octet_string *octet_strncpy(octet_string *dest, const octet_string *src, uint32_t n);
+
+#endif /* _OCTET_STRING_H_ */
